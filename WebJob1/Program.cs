6dbfbb68492b1f2b512c0591b8a9ad6cae2d59d2
dipali.dom <dipali.dom@GGKU3SYS213>
2017-04-04 18:28:44 +0530

    Add project files.

---
+++ WebJob1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
+
+namespace WebJob1
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var _storageConn = ConfigurationManager
+           .ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
+
+            var _dashboardConn = ConfigurationManager
+                .ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
+
+            var _serviceBusConn = ConfigurationManager
+                .ConnectionStrings["AzureWebJobsServiceBus"].ConnectionString;
+
+            JobHostConfiguration config = new JobHostConfiguration();
+            config.StorageConnectionString = _storageConn;
+            config.DashboardConnectionString = _dashboardConn;
+            config.ServiceBusConnectionString = _serviceBusConn;
+            JobHost host = new JobHost(config);
+            host.RunAndBlock();
+        }
+    }
+}
